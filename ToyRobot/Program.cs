4fc2c55fe77d88d7a3d14b40544002ff41dad007
Jesse Bellingham <jbellingham91@gmail.com>
2023-08-18 20:43:39 +1000

    Read input from a file

--- ToyRobot/Program.cs
+++ ToyRobot/Program.cs
@@ -1,4 +1,7 @@
-﻿using ToyRobot.Commands;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using ToyRobot.Commands;
 
 namespace ToyRobot
 {
@@ -8,13 +11,44 @@ namespace ToyRobot
         {
             var robot = new Robot();
             var board = new Board(robot);
-            ICommand command = new Place(board, new Position(Facing.East, 3, 3));
-            command.Execute();
 
-            command = new Move(board, robot);
-            command.Execute();
-            command.Execute();
-            command.Execute();
+            var lines = File.ReadLines("input/in2.txt");
+            foreach (var line in lines)
+            {
+                var split = line.Split(' ');
+                var commandString = split[0];
+                ICommand command;
+                switch (commandString)
+                {
+                    case "PLACE":
+                        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+                        var positionSplit = split[1].Split(',');
+                        var facingString = textInfo.ToTitleCase(positionSplit[2].ToLower());
+                        
+                        command = new Place(
+                            board,
+                            new Position(
+                                Enum.Parse<Facing>(facingString),
+                                Int32.Parse(positionSplit[0]),
+                                Int32.Parse(positionSplit[1])));
+                        break;
+                    case "MOVE":
+                        command = new Move(board, robot);
+                        break;
+                    case "LEFT":
+                        command = new Left(robot);
+                        break;
+                    case "RIGHT":
+                        command = new Right(robot);
+                        break;
+                    case "REPORT":
+                        command = new Report(robot);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Input document contained an unexpected command.");
+                }
+                command.Execute();
+            }
         }
     }
 }
